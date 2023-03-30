9c1ddb8e8aeb1f022c165f0266bacbb322f8d609
dariusSch3ll <darius.scheller@gmail.com>
2023-03-30 19:13:17 +0200

    Make it Compatible to build and execute Jar File

---
+++ src/main/java/application/betweentwo/App.java
@@ -0,0 +1,36 @@
+package application.betweentwo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.*;
+
+
+public class App extends Application{
+	
+	//Created Logging
+	private static Logger applicationlogger = LogManager.getLogger(App.class.getName());
+	
+	public static void main(String[] args) {
+		applicationlogger.info("Launching Main Class");
+		launch(args);
+	}
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		applicationlogger.info("searching layout.fxml...");
+		Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
+		applicationlogger.info("layout.fxml has been found!");
+		applicationlogger.info("Building scene...");
+        Scene scene = new Scene(root, 800, 400);
+    
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        applicationlogger.info("Program has launched!");
+	}
+
+}
--- src/main/java/application/betweentwo/Main.java
+++ src/main/java/application/betweentwo/Main.java
@@ -1,35 +1,16 @@
 package application.betweentwo;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+
 import org.apache.logging.log4j.*;
 
 
-public class Main extends Application {
+public class Main {
 
 	//Created Logging
 	private static Logger applicationlogger = LogManager.getLogger(Main.class.getName());
 	
 	public static void main(String[] args) {
 		applicationlogger.info("Launching Main Class");
-		launch(args);
+		App.main(args);
 	}
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		applicationlogger.info("searching layout.fxml...");
-		Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
-		applicationlogger.info("layout.fxml has been found!");
-		applicationlogger.info("Building scene...");
-        Scene scene = new Scene(root, 800, 400);
-    
-        primaryStage.setTitle("FXML Welcome");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        applicationlogger.info("Program has launched!");
-	}
-
 }
